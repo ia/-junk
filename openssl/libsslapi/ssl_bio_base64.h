a940412c8eec4238c01a3a9e2972c182abbccb1e
Ivan Zorin <ivan.a.zorin@gmail.com>
2011-09-20 20:15:08 +0400

    libsslapi: ssl_bio_base64: implement basic ssl_bio_base64 encrypt/decrypt buffer

--- openssl/libsslapi/ssl_bio_base64.h
+++ openssl/libsslapi/ssl_bio_base64.h
@@ -1,6 +1,13 @@
 
-int ssl_bio_base64_encrypt_buffer();
-int ssl_bio_base64_decrypt_buffer();
+#include <stdio.h>
+#include <string.h>
+
+#include <openssl/buffer.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+
+int ssl_bio_base64_encrypt_buffer(char *plaintext);
+int ssl_bio_base64_decrypt_buffer(char *ciphertext);
 
 int ssl_bio_base64_encrypt_file();
 int ssl_bio_base64_decrypt_file();
--- openssl/libsslapi/sslapi.c
+++ openssl/libsslapi/sslapi.c
@@ -5,6 +5,9 @@ int main(int argc, const char *argv[])
 {
 	ssl_evp_aes_encrypt_buffer("abc\n", "a", 0);
 	
+	ssl_bio_base64_encrypt_buffer("a\n");
+	ssl_bio_base64_decrypt_buffer("YQo=\n");
+
 	char *cipher = "";
 	ssl_evp_aes_decrypt_buffer("password", cipher, 0);
 	
