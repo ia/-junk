a62efc1b6c07b37c25938eb6307a78c522b6c4a3
Ivan Zorin <ivan.a.zorin@gmail.com>
2012-01-24 19:54:32 +0400

    tests/: add more pointer snippets in ptr.c

--- tests/ptr.c
+++ tests/ptr.c
@@ -1,11 +1,42 @@
 
 #include <stdio.h>
 
+/*
+st s
+s.j
+s.k
+
+st *s
+s->j = 10
+
+st s
+s.i 
+*s.i
+*/
 struct st {
 	int j;
 	int *i;
+	char *string;
 	int k;
 };
+
+/*
+int ptr_chr2(char *string)
+{
+	string = malloc(10);
+	memset(string, '0', 9);
+	return 0;
+}
+*/
+//int ptr_chr(long *string)
+int ptr_chr(char **string)
+{
+	//printf("%s\n", string);
+	*string = malloc(32);
+	memset(*string, '0', 31);
+	printf("0x%lX\t0x%X\n", *string, (char)*string);
+	return 0;
+}
 
 int ptr(int *a)
 {
@@ -20,22 +51,26 @@ int ptr_st(struct st *s)
 	printf("\nPTR\n");
 	printf("ctx addr: %x\n", s);
 	printf("ctx.clen addr: %x\n", &(s->i));
-	ptr( &(s->i) );
-	//ptr(s->i);
+	//ptr( &(s->i) );
+	ptr(s->i);
+	ptr_chr(&(s->string));
+	printf("%s\n", s->string);
 	return 0;
 }
 
 int test()
 {
+	int num = 10;
 	struct st s;
-	s.i = 10;
-
+	s.i = &num;
+	//char *str = "alsdkf";
+	//s.string = str;
 	printf("ctx addr: %x\n", &s);
-	printf("ctx.clen addr: %x\n", &s.i);
-	printf("ctx.clen lval: %d\n", s.i);
+	printf("ctx.clen addr: %x\n", s.i);
+	printf("ctx.clen lval: %d\n", *s.i);
 
 	ptr_st(&s);
-	printf("%d\n", s.i);
+	printf("%d\n", *s.i);
 
 /*
 	int a = 10;
